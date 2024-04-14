bca95a0b3f571f9066b30f6ba9a4b541cb242c49
bissan <s12218588@stu.najah.edu>
2024-04-14 11:12:35 +0200

    new edit

--- client/src/components/Layouts/Header/Header.js
+++ client/src/components/Layouts/Header/Header.js
@@ -1,6 +1,6 @@
 import logo from '../images/logo.png';
 import "./header.css";
-import React, { useState } from 'react';
+import React, { useRef,useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
@@ -9,7 +9,13 @@ function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
-
+   const navbarLinks = [
+    {title:"HOME", link : "#"},
+    {title:"BLOG", link : "#"},
+    {title:"HELP CENTER", link : "#"},
+    {title:"ABOUT US", link : "#"},
+    {title:"CONTACT", link : "#"}
+   ]
   return (
     <div className='container2'>
       <Navbar bg="body-tertiary" expand="lg">
@@ -20,14 +26,14 @@ function Header() {
 
           </Navbar.Brand>
           <div id="demoFont">Warzone Elite</div>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleMenu} />
-          <Navbar.Collapse in={isOpen}>
-            <Nav className="me-auto mb-2 mb-lg-0" id="navitem">
-              <Nav.Link href="#">HOME</Nav.Link>
-              <Nav.Link href="#">BLOG</Nav.Link>
-              <Nav.Link href="#">HELP CENTER</Nav.Link>
-              <Nav.Link href="#">ABOUT US</Nav.Link>
-              <Nav.Link href="#">CONTACT</Nav.Link>
+          <Navbar.Toggle style={{ color: 'white' }} aria-controls="basic-navbar-nav"  onClick={toggleMenu} />
+          <Navbar.Collapse in={isOpen} className="custom-toggle">
+            <Nav className="me-auto mb-2 mb-lg-0" id="navitem" >
+          
+              {navbarLinks.map((item,index)=>{
+
+                return(<Nav.Link key={index} href={item.link} >{item.title} </Nav.Link>)
+              })}
             </Nav>
           </Navbar.Collapse>
           <form className="cc" role="search">
