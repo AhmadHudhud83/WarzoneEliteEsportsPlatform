b7a090f86d11d4e88d3089f84b58b7506f5422ed
Ahmad Hudhud <ahmadhudhud1212@gmail.com>
2024-04-22 23:46:10 +0300

    welcome from dev branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,54 @@
-
-
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-
-
-import { RoutesManger } from "./setup/routes-manger/RoutesManger.js";
-import React from "react";
+import Footer from "./components/Layouts/Footer/Footer";
+import axios from "axios"; //INSTALLING AXIOS PACKAGE FOR API CALLING => npm i axois (in the project directory not the client***)
+import SelectGame from "./pages/create_tournement_select_game/SelectGame";
+import Header from './components/Layouts/Header/Header.js'
 function App() {
-  
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  //FETCH THE BACKEND API
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
   //react fragment is the best practice
   return (
-  <React.Fragment>
-    <RoutesManger></RoutesManger>
-  </React.Fragment>
-     
+    <BrowserRouter>
+      <React.Fragment>
+      <Header/>
+
+        <h1>hello esports team</h1>
+        <ul>
+          {(typeof backendData.users === 'undefined') ? (
+            <p>Loading...</p>
+          ) : (
+            backendData.users.map((user, i) => {
+              return (<li key={i}>{user}</li>)
+            })
+          )}
+        </ul>
+        <p>press f12 to see the data from mongoDB database</p>
+        <Footer />
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
