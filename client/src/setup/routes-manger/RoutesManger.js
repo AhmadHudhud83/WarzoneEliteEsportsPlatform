d3c0948c00acda8459c30838018bda335735232d
Ahmad Hudhud <ahmadhudhud1212@gmail.com>
2024-04-22 18:44:19 +0300

    nothing

--- client/src/setup/routes-manger/RoutesManger.js
+++ client/src/setup/routes-manger/RoutesManger.js
@@ -12,6 +12,7 @@ return(<BrowserRouter>
         <Route path="/">
           <Route index element={<Temp/>} />
           <Route path="create-tournement" element={<SelectGame />} />
+          
           <Route path="*" element={<h1>Error 404 ! Page not found</h1>} /> 
         </Route>
       </Routes>
