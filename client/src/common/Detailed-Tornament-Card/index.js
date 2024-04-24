953747d0bd24fdfa2d0b539549e3865d4b2c2aab
Ahmad Hudhud <ahmadhudhud1212@gmail.com>
2024-04-24 15:47:27 +0300

    adjusted navigation on the tournament card

--- client/src/common/Detailed-Tornament-Card/index.js
+++ client/src/common/Detailed-Tornament-Card/index.js
@@ -4,11 +4,7 @@ import { Overview } from "./top-navbar-components/Overview/Overview";
 import { Participants } from "./top-navbar-components/Participants/Participants";
 import { Brackets } from "./top-navbar-components/Brackets/Brackets";
 import { Announcements } from "./top-navbar-components/Announcements/Announcements";
-import { Details } from "./bottom-navbar-components/Details/Details";
-import { Prizes } from "./bottom-navbar-components/Prizes/Prizes";
-import { Rules } from "./bottom-navbar-components/Rules/Rules";
-import { Schedule } from "./bottom-navbar-components/Schedule/Schedule";
-import { Contact } from "./bottom-navbar-components/Contact/Contact";
+
 import { ManagementButton } from "./managementButton/ManagementButton";
 
 
@@ -23,11 +19,6 @@ export const DetailedTournamentCard = ()=>{
 
 
 
-  const [activeBottomComponent, setActiveBottomComponent] = useState(0  );
-
-  const handleBottomComponent = (componentNumber) => {
-    setActiveBottomComponent(componentNumber);
-  };
 
 
 
@@ -44,32 +35,19 @@ export const DetailedTournamentCard = ()=>{
 ]
 
 
-const bottomNavElements = [
-    {link:"#",element:"DETAILS"},
-    {link:"#",element:"RULES"},
-    {link:"#",element:"PRIZES"},
-    {link:"#",element:"SCHEDULE"},
-    {link:"#",element:"CONTACT"}
-    
 
-
-
-]
 const [activeTopNav,setActiveTopNav]=useState(0)
 const handleActiveTopNav=(i)=>{
     setActiveTopNav(i)
 }
 
 
-const [activeBottomNav,setActiveBottomNav]=useState(0)
-const handleActiveBottomNav=(i)=>{
-    setActiveBottomNav(i)
-}
+
 
 const x = `nav-link text-muted`
 return(<React.Fragment>
 
-<div  className="container ">
+<div  className="container org-cont " >
 <h2 className="text-start  pb-3" >{tournament_name}</h2>
 <div className="card text-center text-white bg-secondary cont-1  " >
   <div className="card-header border ">
@@ -93,40 +71,14 @@ return(<React.Fragment>
     
     </div>
     {activeTopComponent===0 && <Overview></Overview>}
-    {activeTopComponent===1 &&<Participants></Participants>}
-    {activeTopComponent===2&&<Brackets></Brackets>}
-    {activeTopComponent===3&&<Announcements></Announcements>}
+    {activeTopComponent===1 && <Participants></Participants>}
+    {activeTopComponent===2 && <Brackets></Brackets>}
+    {activeTopComponent===3 && <Announcements></Announcements>}
     
     
     
   </div>
-  <div className="card-header border">
-    <ul className="nav nav-pills card-header-pills my-3 d-flex justify-content-center ">
-   
-
-
-    {bottomNavElements.map((item,index)=>{
-        return(
-            <li  key ={index} className={`nav-item  ${activeBottomNav === index ? 'active' : ''}`}>
-            <a  onClick={() => {
-               
-                handleBottomComponent(index);
-                handleActiveBottomNav(index)
-            }} className={`nav-link ${activeBottomNav === index ?'text-white':'text-muted'}`} href={item.link}>{item.element}</a>
-          </li>
-        )
-    })}
-    
-    </ul>
-
-    {activeBottomComponent===0 && <Details></Details>}
-    {activeBottomComponent===1 &&<Rules></Rules>}
-    {activeBottomComponent===2&&<Prizes></Prizes>}
-    {activeBottomComponent===3&&<Schedule></Schedule>}
-    {activeBottomComponent===4&&<Contact/>}
-    
-    
-  </div>
+  
   
 
   
--- client/src/common/Detailed-Tornament-Card/top-navbar-components/Participants/Participants.js
+++ client/src/common/Detailed-Tornament-Card/top-navbar-components/Participants/Participants.js
@@ -1,16 +1,43 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export const Participants =()=>{
-const TeamsArr=["team1","team2","team3"]
+const TeamsArr=[
+    {name:"TeamOne",no:"1"},
+    {name:"TeamTwo",no:"2"},
+    {name:"TeamThree",no:"3"},
+    {name:"TeamFour",no:"4"},
+]
 
 return (<React.Fragment>
-
-    <h5 className="text-start m-4">Teams</h5>
-    <div className="border ">
-    <li class="list-group-item bg-secondary">An item</li>
-    <li class="list-group-item bg-secondary">An item</li> <li class="list-group-item bg-secondary">An item</li>
-    <li class="list-group-item bg-secondary">An item</li>
+<div className="d-flex m-4"><h5 className="text-start">Teams</h5>
+    <button type="button" className="btn btn-primary ms-auto">+Add Teams</button></div>
+    
  
+    <div className="border ">
+   <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Reg#</th>
+      <th scope="col">Team</th>
+      <th scope="col"># of Players</th>
+     
+    </tr>
+  </thead>
+  <tbody>
+
+    {(typeof TeamsArr[0] ==='undefined') ?  (<h1 className="ms-auto">No available teams</h1>):  TeamsArr.map((e)=>{
+        return(<tr>
+<th scope="row">{e.no}</th>
+<td>{e.name}</td>
+<td>{e.no}</td>
+        </tr>)
+    })}
+   
+  
+  </tbody>
+</table>
+
 
 
     </div>
