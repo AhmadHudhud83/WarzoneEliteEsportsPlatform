c6f71687998c35b9b158f3e37d7d0db0c6721cb9
Ahmad Hudhud <ahmadhudhud1212@gmail.com>
2024-04-29 14:59:57 +0300

    corrected folders path

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getAllGames } = require("../Controller/gamesController");
+const { getAllGames } = require("../controllers/gamesController");
 
 router.get("/", (req, res) => {
     getAllGames(req, res)
