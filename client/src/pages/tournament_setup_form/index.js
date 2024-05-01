cc13c42f4c3341423c3bfd18f5fc5ddff1537d42
Ahmad Hudhud <ahmadhudhud1212@gmail.com>
2024-05-01 18:18:02 +0300

    styling

--- client/src/pages/tournament_setup_form/index.js
+++ client/src/pages/tournament_setup_form/index.js
@@ -1,32 +1,85 @@
 import React from "react"
+import { useState } from "react";
+export const TournamentSetupForm=(props)=>{
 
-export const TournamentSetupForm=()=>{
 
+    const [activeTopComponent, setActiveTopComponent] = useState(0);
 
-
+    const handleTopComponent = (componentNumber) => {
+      setActiveTopComponent(componentNumber);
+    };
+    
+    
+    
+    
+    
+    
+     
+      const topNavElements = [
+          {link:"#",element:"BASICS"},
+          {link:"#",element:"INFO"},
+          {link:"#",element:"SETTINGS"},
+          {link:"#",element:"PUBLISH"},
+          
+    
+    ]
+    
+    
+    
+    const [activeTopNav,setActiveTopNav]=useState(0)
+    const handleActiveTopNav=(i)=>{
+      setActiveTopNav(i)
+    }
+    
 
 
     return(<React.Fragment>
+         
         
-        
-   <form>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" />
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
 
-        
+
+
+  <div  className="container pt-5 mt-5 org-cont " >
+
+<div className="card  text-white bg-secondary cont-1  " >
+  <div className="card-header border ">
+    <div className="border-bottom">
+    <ul className="nav nav-pills card-header-pills my-3 py-2 d-flex justify-content-center">
+   
+    {topNavElements.map((item,index)=>{
+        return(
+            <li  key ={index} className={`nav-item fs-3   ${activeTopNav === index ? 'active border-bottom border-4 border-danger' : ''}`}>
+            <a  onClick={() =>{
+           handleTopComponent(index);
+            handleActiveTopNav(index)}
+            } className={`nav-link ${activeTopNav === index ? 'text-white' : 'text-muted'}`} href={item.link}>{item.element}</a>
+          </li>
+        )
+    })}
+    
+    {props.children}
+   
+    </ul>
+    
+    </div>
+    {activeTopComponent===0 && <h1>Hello world 1</h1>}
+    {activeTopComponent===1 && <h1>Hello world 2</h1>}
+    {activeTopComponent===2 && <h1>Hello world 3</h1>}
+    {activeTopComponent===3 && <h1>Hello world 4</h1>}
+    
+    
+    
+  </div>
+  
+  
+
+  
+ 
+</div>
+</div>  
+
+
+
         
         </React.Fragment>)
 }
