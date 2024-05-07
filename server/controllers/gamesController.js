5732d4eb29540f17a55206687010afa85c4f8aeb
Ahmed Adel <116980730+AhmedAdel2003@users.noreply.github.com>
2024-05-07 13:19:53 +0300

    CRUD op for game

--- server/controllers/gamesController.js
+++ server/controllers/gamesController.js
@@ -1,15 +1,72 @@
 import { GameModel } from "../models/Games.js";
+import { body, query } from "express-validator";
 
-
-
-export const getAllGames = async (req, res) => {
+const getAllGames = async (req, res) => {
     try {
         const games = await GameModel.find();
         res.json(games);
-        
+
     } catch (error) {
         console.error("Error fetching games:", error);
         res.status(500).json({ error: "Failed to retrieve games" });
     }
 };
+const AddGame = async (req, res) => {
+    const {  key, name, imgUrl } = req.body;
+    try {
+        const newGame = new GameModel({
+                key: key,
+                name: name,
+                imgUrl: imgUrl
+        });
+        const result = await newGame.save();
+        res.status(201).json(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
 
+    }
+};
+
+
+const UpdateGame = async (req, res) => {
+    const { id, key, name, imgUrl } = req.body;
+    const UpdateGame = await GameModel.updateOne(
+        { _id: id },
+        {
+            key: key,
+            name: name,
+            imgUrl: imgUrl
+        }
+
+    )
+    if (UpdateGame.modifiedCount != 0) {
+        res.send("true");
+    }
+    else {
+        res.send("false")
+    }
+};
+
+
+
+const DeleteGame = async (req, res) => {
+    const { id } = req.query;
+    try {
+        const Delete = await GameModel.deleteOne({ _id: id });
+        console.log(Delete);
+        if (Delete.deletedCount != 0) {
+            return res.status(200).send("true");
+        }
+        else {
+            return res.status(404).send("user not found");
+        }
+
+    }
+    catch (e) {
+        return res.status(500).send("System error Deleting", e);
+    }
+
+}
+
+export { getAllGames, AddGame, UpdateGame, DeleteGame };
--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -1,12 +1,27 @@
-import  express  from "express";
-
-import { getAllGames } from "../controllers/gamesController.js";
+import express from "express";
+import { getAllGames, AddGame, UpdateGame, DeleteGame } from "../controllers/gamesController.js";
 const routerGame = express.Router();
+import { body, query } from "express-validator";
 
 routerGame.get("/Games", (req, res) => {
-    getAllGames(req, res)
+    getAllGames(req, res);
+});
+
+//create a new game
+routerGame.post("/game/add", (req, res) => {
+    AddGame(req, res);
+});
+
+//update a game
+routerGame.put("/game/update", (req, res) => {
+    UpdateGame(req, res);
+});
+
+//delete a game
+routerGame.delete("/game/delete", (req, res) => {
+    DeleteGame(req, res);
 });
 
 
 
-export {routerGame};
+export { routerGame };
