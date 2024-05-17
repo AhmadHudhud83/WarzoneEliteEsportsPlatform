82381661d145df1cdefd9893ab2d679a668149ee
AhmadHudhud83 <151885597+AhmadHudhud83@users.noreply.github.com>
2024-05-17 10:02:09 +0300

    fixed the hardcoded cover_image_url function

--- server/controllers/tournamentCotroller.js
+++ server/controllers/tournamentCotroller.js
@@ -1,6 +1,7 @@
 import { TouranmentModel } from "../models/Tournaments.js";
 import Joi from "joi"; //for a better valdiation
 import fs from "fs"
+import axios from "axios"
 /**
  * @desc Create a new tournament
  * @route /api/tournaments
@@ -9,7 +10,25 @@ import fs from "fs"
  *
  */
 export const createTournament = async (req, res) => {
-  //custom empty or undefined data validation function
+
+  //get games from mongoDB request
+   const getCoverImageUrl =async (gameName)=>{
+    let defaultImage = "https://i.imgur.com/KHneQTJ.png"
+    try{
+      const respones = await axios.get("http://localhost:5000/api/Games")
+      const games =respones.data;
+      const game = games.find(g=>g.name===gameName
+       
+      )
+      if(game){
+        return game.imgUrl
+      }
+      console.log("gameResponse DATA is : ",gameResponse.data)
+    }catch(err){
+      console.error("ERROR",err)
+    }
+    return defaultImage
+  }
 
   console.log("body: ", req.body);
   console.log("file: ", req.file);
@@ -33,50 +52,9 @@ export const createTournament = async (req, res) => {
   //==================JOI VALIDATION (EXPRESS LEVEL VALIDAITON)=====================
   //parse the sponsors array of objects
   req.body.sponsors = parsedSponsors;
-  // if there is no tournament cover uploaded , then there is a default image cover accroding to the game selected
-  const defaultTournamentCoverImage = () => {
-    //1st scenario : cover_image_url is undefined or not sent due to any reason,set to the default tournament banner of the website
-    if (!req.body.cover_image_url) {
-      return "https://i.imgur.com/KHneQTJ.png";
-    } else if (req.body.game === "CS2") {
-      return "https://i.imgur.com/7lr8WWw.png";
-    } else if (req.body.game === "Valorant") {
-      return "https://seeklogo.com/images/V/valorant-logo-FAB2CA0E55-seeklogo.com.png";
-    } else if (req.body.game === "Dota 2") {
-      return "https://i.redd.it/e8k5dhz958va1.jpg";
-    } else if (req.body.game === "League of Legends") {
-      return "https://images.prismic.io/play-vs/6c423286e877921fb6659122b16e1845df833e1f_league-of-legends-hero-splash.jpg?auto=compress,format";
-    } else if (req.body.game === "Overwatch") {
-      return "https://images.nintendolife.com/1558f6cd6d6fe/overwatch-2-cover.cover_large.jpg";
-    } else if (req.body.game === "Rainbow Six Siege") {
-      return "https://cdn.vox-cdn.com/thumbor/dOyerwaJUC0uadaE18Ll-lst4K4=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/16000420/rainbow_six_siege_elite_skins_hero.jpg";
-    } else if (req.body.game === "Rocket League") {
-      return "https://upload.wikimedia.org/wikipedia/commons/e/e0/Rocket_League_coverart.jpg";
-    } else if (req.body.game === "Fortnite") {
-      return "https://cdn.vox-cdn.com/thumbor/fvf8xI-bdpHhUwO_O3QyhLhlt7M=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/21822797/Fortnite_C2S4_BPLineup.jpg";
-    } else if (req.body.game === "PUBG") {
-      return "https://assetsio.gnwcdn.com/pubg-tips-tricks-guide-battlegrounds-4415-1513099928095.jpg?width=1200&height=1200&fit=crop&quality=100&format=png&enable=upscale&auto=webp";
-    } else if (req.body.game === "Apex Legends") {
-      return "https://image.api.playstation.com/vulcan/ap/rnd/202401/1810/63e55900b867e1d9e50bf139f92fe1fe94e9a048503990d3.png";
-    } else if (req.body.game === "Hearthstone") {
-      return "https://sm.ign.com/t/ign_it/screenshot/default/hearthstone_fxgu.1280.jpg";
-    } else if (req.body.game === "Smite") {
-      return "https://cdn1.epicgames.com/offer/16ed9f15b1b449ccb59cb610b13df5b8/EPIC-MarketingSizes-1r1-1920x1080_1920x1080-2b0de61ac94515759c64f914188ca9b5?h=270&quality=medium&resize=1&w=480";
-    } else if (req.body.game === "World of Warcraft") {
-      return "https://assetsio.gnwcdn.com/World-Of-Warcraft-Wrath-Of-The-Lich-King.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp";
-    } else if (req.body.game === "Starcraft 2") {
-      return "https://bnetcmsus-a.akamaihd.net/cms/blog_header/ci/CIGT53U8ZP6M1509744317189.jpg";
-    } else if (req.body.game === "FIFA") {
-      return "https://cdn.akamai.steamstatic.com/steam/apps/1506830/capsule_616x353.jpg?t=1712678728";
-    } else if (req.body.game === "NBA 2K") {
-      return "https://e0.365dm.com/22/09/1600x900/skysports-nba-nba-2k23_5915425.jpg?20220930185017";
-    } else if (req.body.game === "Madden") {
-      return "https://image.api.playstation.com/vulcan/ap/rnd/202402/2715/888de44bc0feb81861b9b7fe789c077df1de1e803e01a848.png";
-    } else return "https://i.imgur.com/KHneQTJ.png"; //same for the game name (if not provided)
-  };
+  //setting the cover_image_url
 
-    req.body.cover_image_url = defaultTournamentCoverImage();
-
+  req.body.cover_image_url =await getCoverImageUrl(req.body.game)
   const { error } = tournamentValidation(req.body, "POST");
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
