f0c415b16ca2f519c659bd402db82901664154af
AhmadHudhud83 <151885597+AhmadHudhud83@users.noreply.github.com>
2024-05-17 10:04:49 +0300

    ...

--- server/controllers/tournamentCotroller.js
+++ server/controllers/tournamentCotroller.js
@@ -1,7 +1,7 @@
 import { TouranmentModel } from "../models/Tournaments.js";
 import Joi from "joi"; //for a better valdiation
-import fs from "fs"
-import axios from "axios"
+import fs from "fs";
+import axios from "axios";
 /**
  * @desc Create a new tournament
  * @route /api/tournaments
@@ -10,25 +10,22 @@ import axios from "axios"
  *
  */
 export const createTournament = async (req, res) => {
-
   //get games from mongoDB request
-   const getCoverImageUrl =async (gameName)=>{
-    let defaultImage = "https://i.imgur.com/KHneQTJ.png"
-    try{
-      const respones = await axios.get("http://localhost:5000/api/Games")
-      const games =respones.data;
-      const game = games.find(g=>g.name===gameName
-       
-      )
-      if(game){
-        return game.imgUrl
+  const getCoverImageUrl = async (gameName) => {
+    let defaultImage = "https://i.imgur.com/KHneQTJ.png";
+    try {
+      const respones = await axios.get("http://localhost:5000/api/Games");
+      const games = respones.data;
+      const game = games.find((g) => g.name === gameName);
+      if (game) {
+        return game.imgUrl;
       }
-      console.log("gameResponse DATA is : ",gameResponse.data)
-    }catch(err){
-      console.error("ERROR",err)
+     // console.log("gameResponse DATA is : ", response.data);
+    } catch (err) {
+      console.error("ERROR", err);
     }
-    return defaultImage
-  }
+    return defaultImage;
+  };
 
   console.log("body: ", req.body);
   console.log("file: ", req.file);
@@ -54,7 +51,7 @@ export const createTournament = async (req, res) => {
   req.body.sponsors = parsedSponsors;
   //setting the cover_image_url
 
-  req.body.cover_image_url =await getCoverImageUrl(req.body.game)
+  req.body.cover_image_url = await getCoverImageUrl(req.body.game);
   const { error } = tournamentValidation(req.body, "POST");
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
@@ -217,34 +214,33 @@ export const getTournamentById = async (req, res) => {
  */
 export const updateTournament = async (req, res) => {
   //  const newReqBody = emptyDataValidation(req.body);
-  const parsedSponsors = JSON.parse(req.body.sponsors)
-  delete req.body._id
-  delete req.body.createdAt
-  delete req.body.updatedAt
-  delete req.body.__v
-  console.log(req.file)
-  req.body.sponsors=parsedSponsors
+  const parsedSponsors = JSON.parse(req.body.sponsors);
+  delete req.body._id;
+  delete req.body.createdAt;
+  delete req.body.updatedAt;
+  delete req.body.__v;
+  console.log(req.file);
+  req.body.sponsors = parsedSponsors;
   const { error } = tournamentValidation(req.body, "PUT");
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
   //find the old tournament data to remove the old image cover if a newer cover has been uploaded (with file system package)
-  if(req.file){ // when any file (image) uploaded , then delete the old image cover
-    try{
-    
-      const oldTournament= await TouranmentModel.findById(req.params.id)
-      if(!oldTournament){
-        res.status(404).json({message:"old tournament not found"})
+  if (req.file) {
+    // when any file (image) uploaded , then delete the old image cover
+    try {
+      const oldTournament = await TouranmentModel.findById(req.params.id);
+      if (!oldTournament) {
+        res.status(404).json({ message: "old tournament not found" });
       }
-      if(oldTournament.cover_image_url){
-        fs.unlinkSync(oldTournament.cover_image_url)
+      if (oldTournament.cover_image_url) {
+        fs.unlinkSync(oldTournament.cover_image_url);
       }
-    }catch(err){
-      console.log("old tournament error",err)
+    } catch (err) {
+      console.log("old tournament error", err);
     }
-      
   }
-  
+
   //================================================================
   try {
     const tournament = await TouranmentModel.findByIdAndUpdate(
@@ -265,7 +261,7 @@ export const updateTournament = async (req, res) => {
           description: req.body.description,
           tournament_status: req.body.tournament_status,
           registeration_status: req.body.registeration_status,
-          cover_image_url: req.file? req.file.path : req.body.cover_image_url,
+          cover_image_url: req.file ? req.file.path : req.body.cover_image_url,
           announcements: req.body.announcements,
           sponsors: req.body.sponsors,
           supervisors: req.body.supervisors,
@@ -273,7 +269,7 @@ export const updateTournament = async (req, res) => {
       },
       { new: true }
     );
-    console.log(req.body.cover_image_url)
+    console.log(req.body.cover_image_url);
     res.status(200).json(tournament);
   } catch (error) {
     console.error(error);
