3e0a857d76d3be7bb8ae4c6e08ec33af156afd52
Ahmed Adel <116980730+AhmedAdel2003@users.noreply.github.com>
2024-05-24 18:41:45 +0300

    make some edit
    make some edit and add report buton

--- client/src/common/Detailed-Tornament-Card/RegistrationCard/RegistrationCard.js
+++ client/src/common/Detailed-Tornament-Card/RegistrationCard/RegistrationCard.js
@@ -1,9 +1,18 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
+
 import { useTournamentDetails } from "..";
 const RegistrationCard = () => {
   const tournament = useContext(useTournamentDetails);
+  const [showReportBox, setShowReportBox] = useState(false);
 
+  const handleReportClick = () => {
+    setShowReportBox(true);
+  };
+
+  const handleCloseReportBox = () => {
+    setShowReportBox(false);
+  };
   return (
     <React.Fragment>
       <div
@@ -18,17 +27,51 @@ const RegistrationCard = () => {
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
+
           <h4>
             {tournament.participants.length}/{tournament.max_participants}{" "}
             Participants
           </h4>
-          <button
-            className="btn btn-danger my-4"
-            disabled={tournament.registeration_status === "Closed"}
-          >
-            Join Tournament
-          </button>
+
+          <div className="btn-group">
+            <button type="button" className="btn btn-danger">
+              Report
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger dropdown-toggle dropdown-toggle-split"
+              data-bs-toggle="dropdown"
+              aria-expanded={showReportBox}
+            >
+              <span className="visually-hidden">Toggle Dropdown</span>
+            </button>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" >Action</a></li>
+              <li><a className="dropdown-item" >Another action</a></li>
+              <li><a className="dropdown-item" >Something else here</a></li>
+              <li>
+                <a className="dropdown-item" onClick={handleReportClick}>
+                  Other ...
+                </a>
+              </li>
+            </ul>
+          </div>
+          {showReportBox && (
+            <div className="report-box">
+              <textarea placeholder="Write your report here..." />
+              <button onClick={handleCloseReportBox}>Close</button>
+            </div>
+          )}
+
         </div>
+
+        <button
+          className="btn btn-danger "
+          disabled={tournament.registeration_status === "Closed"}
+        >
+          Join Tournament
+        </button>
+
       </div>
     </React.Fragment>
   );
