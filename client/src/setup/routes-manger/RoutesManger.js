426ee1d40334eaf73a000124a06bade6cb747557
osama-sholi <117115944+osama-sholi@users.noreply.github.com>
2024-05-28 19:44:31 +0300

    simple login signup design

--- client/src/setup/routes-manger/RoutesManger.js
+++ client/src/setup/routes-manger/RoutesManger.js
@@ -23,7 +23,8 @@ import AnnouncementsPage from "../../pages/organizer_dashboard/components/Announ
 import GameList from "../../pages/organizer_dashboard/components/Games/ListGame";
 import OrganizerLogin from "../../pages/login/OrganizerLogin";
 import SupervisorLogin from "../../pages/login/SupervisorLogin";
-
+import PlayerLogin from "../../pages/login/PlayerLogin.js";
+import PlayerSignup from "../../pages/login/PlayerSignup";
 //tournament form flag
 const createTournamentRequest = "CREATE_TOURNAMENT"; //for creating tournament form handling
 const updateTournamentRequest = "UPDATE_TOURNAMENT"; //for updating exisiting tournament form handling
@@ -35,59 +36,29 @@ const RoutesManger = () => {
       <Routes>
         <Route path="/">
           <Route index element={<HomePage />} />
-          
-
-            <Route path="organizer/">
-              <Route index element={<OrganizerLogin />} />
-              <Route path="dashboard/">
-                <Route index element={<OrganizerDashboard />} />
-                <Route path="supervisors/">
-                  <Route index element={<SupervisorList />} />
-                  <Route path="add" element={<AddSupervisor />} />
-                  <Route
-                    path="edit/:supervisorId"
-                    element={<EditeSupervisor />}
-                  />
-                </Route>
-
-                <Route path="games/">
-                  <Route index element={<GameList/>} />
-                  <Route path="add" element={<h1>addgame</h1>} />
-                  <Route
-                    path="edit/:gameId"
-                    element={<h1>editegame</h1>}
-                  />
-                </Route>
-                <Route path="players/">
-                  <Route index element={<PlayerList />} />
-                  <Route path="add" element={<AddPlayer />} />
-                  <Route path="edit/:playerId" element={<EditePlayer />} />
-                </Route>
-                <Route path="select-game/">
-                  <Route index element={<SelectGame />} />
-                  <Route
-                    path="tournament-setup/:gameName"
-                    element={
-                      <TournamentForm request={createTournamentRequest} />
-                    }
-                  />
-                </Route>
+          <Route path="login" element={<PlayerLogin />} />
+          <Route path="signup" element={<PlayerSignup />} />
+          <Route path="organizer/">
+            <Route index element={<OrganizerLogin />} />
+            <Route path="dashboard/">
+              <Route index element={<OrganizerDashboard />} />
+              <Route path="supervisors/">
+                <Route index element={<SupervisorList />} />
+                <Route path="add" element={<AddSupervisor />} />
                 <Route
                   path="edit/:supervisorId"
                   element={<EditeSupervisor />}
                 />
-                <Route path="announcements/:tournamentId"  element={<AnnouncementsPage/>}/>
-                 <Route
-                path="management/:tournamentId"
-                element={<TournamentForm request={updateTournamentRequest} />}
-              />
-              <Route
-                path="matches/:id"
-                element={<Matches userType="organizer" />}
-              />
               </Route>
-              
-            
+
+              <Route path="games/">
+                <Route index element={<GameList />} />
+                <Route path="add" element={<h1>addgame</h1>} />
+                <Route
+                  path="edit/:gameId"
+                  element={<h1>editegame</h1>}
+                />
+              </Route>
               <Route path="players/">
                 <Route index element={<PlayerList />} />
                 <Route path="add" element={<AddPlayer />} />
@@ -97,41 +68,71 @@ const RoutesManger = () => {
                 <Route index element={<SelectGame />} />
                 <Route
                   path="tournament-setup/:gameName"
-                  element={<TournamentForm request={createTournamentRequest} />}
+                  element={
+                    <TournamentForm request={createTournamentRequest} />
+                  }
                 />
               </Route>
-             
+              <Route
+                path="edit/:supervisorId"
+                element={<EditeSupervisor />}
+              />
+              <Route path="announcements/:tournamentId" element={<AnnouncementsPage />} />
+              <Route
+                path="management/:tournamentId"
+                element={<TournamentForm request={updateTournamentRequest} />}
+              />
+              <Route
+                path="matches/:id"
+                element={<Matches userType="organizer" />}
+              />
             </Route>
 
-            <Route path="supervisor/">
-              <Route index element={<SupervisorLogin/>} />
-              <Route path="tournaments">
-                <Route index element={<Tournaments />} />
-                <Route path=":id" element={<Matches />} />
-              </Route>
+
+            <Route path="players/">
+              <Route index element={<PlayerList />} />
+              <Route path="add" element={<AddPlayer />} />
+              <Route path="edit/:playerId" element={<EditePlayer />} />
+            </Route>
+            <Route path="select-game/">
+              <Route index element={<SelectGame />} />
+              <Route
+                path="tournament-setup/:gameName"
+                element={<TournamentForm request={createTournamentRequest} />}
+              />
             </Route>
 
-            <Route
-              path="tournament-overview/:id"
-              element={<DetailedTournamentCard />}
-            ></Route>
-
-            <Route
-              path="*"
-              element={
-                <>
-                  <h1>Error 404</h1>
-                </>
-              }
-            />
           </Route>
 
-          
-        
-      
-        
+          <Route path="supervisor/">
+            <Route index element={<SupervisorLogin />} />
+            <Route path="tournaments">
+              <Route index element={<Tournaments />} />
+              <Route path=":id" element={<Matches />} />
+            </Route>
+          </Route>
+
+          <Route
+            path="tournament-overview/:id"
+            element={<DetailedTournamentCard />}
+          ></Route>
+
+          <Route
+            path="*"
+            element={
+              <>
+                <h1>Error 404</h1>
+              </>
+            }
+          />
+        </Route>
+
+
+
+
+
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
 
   );
 };
